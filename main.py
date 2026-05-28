a97c74a98d1486786d5d04ac9755a1cb8833c7d1
soulxpressioninc-glitch <soulxpressioninc@gmail.com>
2026-05-28 02:34:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ MONGO_URI = os.environ.get("MONGO_URI")
 def webhook():
     try:
         client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=3000)
-db = client["Rosaeduagent"]        teachers = db["teachers"]
+        db = client["Rosaeduagent"]
+        teachers = db["teachers"]
         req = request.get_json()
         results = list(teachers.find({"improvement_plan_active": {"$ne": False}}, {"_id": 0}))
         if not results:
